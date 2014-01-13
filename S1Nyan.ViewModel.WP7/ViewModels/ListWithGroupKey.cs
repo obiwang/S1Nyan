bf85400a034bb43af4db74aa20dddba56e942e4a
Obi Wang <masterobiwang@gmail.com>
2014-01-13 22:45:21 +0800

    [support wp8]: 1. build & run

---
+++ S1Nyan.ViewModel.WP7/ViewModels/ListWithGroupKey.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace S1Nyan.ViewModels
+{
+    public class ListWithGroupKey<TKey, TValue> : List<TValue>, IGrouping<TKey, TValue>
+    {
+
+        public ListWithGroupKey(TKey key)
+        {
+            Key = key;
+        }
+
+        public ListWithGroupKey(TKey key, IEnumerable<TValue> list)
+            : base(list)
+        {
+            Key = key;
+        }
+
+        public TKey Key { get; private set; }
+    }
+}
--- S1Nyan.ViewModel.WP7/ViewModels/MainPageViewModel.cs
+++ S1Nyan.ViewModel.WP7/ViewModels/MainPageViewModel.cs
@@ -25,6 +25,14 @@ namespace S1Nyan.ViewModels
             _userService = userService;
         }
 
+        private ListWithGroupKey<string, S1ListItem> FormatList(IEnumerable<S1ListItem> list)
+        {
+            foreach (var g in list)
+            {
+                
+            }
+        }
+
         private IEnumerable<S1ListItem> _data = null;
         /// <summary>
         /// Sets and gets the ListData property.
